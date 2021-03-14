8ab4ff5096ed555c660bc6c724bddb7669ad39a8
SpiritedAwayCN <541845219@qq.com>
2021-03-14 18:27:22 +0800

    bootstrap-01

--- helpers/split_traintest.py
+++ helpers/split_traintest.py
@@ -1,21 +1,17 @@
 import os
 import random
 
-TRAIN_LABEL_OUTPUT = 'metadata/train_label.txt'
-TEST_LABEL_OUTPUT = 'metadata/test_label.txt'
-SPLIT_RATIO = 0.1
-DATA_DIR = 'archive'
+TRAIN_LABEL_OUTPUT = '../metadata/train_label_all.txt'
+# TEST_LABEL_OUTPUT = 'metadata/test_label.txt'
+SPLIT_RATIO = 0
+DATA_DIR = '../archive'
 
-with open(TEST_LABEL_OUTPUT, "w") as ftest:
-    with open(TRAIN_LABEL_OUTPUT, "w") as ftrain:
 
-        for cate_name in os.listdir(DATA_DIR):
-            file_list = os.listdir(os.path.join(DATA_DIR, cate_name))
-            random.shuffle(file_list)
+with open(TRAIN_LABEL_OUTPUT, "w") as ftrain:
 
-            test_num = int(len(file_list) * SPLIT_RATIO)
+    for cate_name in os.listdir(DATA_DIR):
+        file_list = os.listdir(os.path.join(DATA_DIR, cate_name))
+        random.shuffle(file_list)
 
-            for i in range(test_num):
-                ftest.write(f'{cate_name}/{file_list[i]} {cate_name}\n')
-            for i in range(test_num, len(file_list)):
-                ftrain.write(f'{cate_name}/{file_list[i]} {cate_name}\n')
+        for i in range(len(file_list)):
+            ftrain.write(f'{cate_name}/{file_list[i]} {cate_name}\n')
